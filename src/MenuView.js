a80ad636f99932aa680e09db9d3444d9482aaff9
bxzahid <bxzahid@gmail.com>
2021-02-06 00:16:05 +0600

    feat(MenuView): added menu view class

---
+++ src/MenuView.js
@@ -0,0 +1,20 @@
+/**
+ * @classdesc Project menu.
+ * @class
+ */
+class Menu {
+	// View menu
+	static menuView() {
+		console.log("\t=======================");
+		console.log("\t1.   Create New Contact");
+		console.log("\t2.   Show All Contact");
+		console.log("\t3.   Update Contact");
+		console.log("\t4.   Delete Contact");
+		console.log("\t5.   Search Contact");
+		console.log("\t6.   Exit");
+		console.log("\t=======================\n");
+		console.log("\tEnter Option: ");
+	}
+}
+
+module.exports = Menu
